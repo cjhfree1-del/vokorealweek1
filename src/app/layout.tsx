448d30d53e7a7a3271bd37e493ab684fc8793908
cjhfree1 <cjhfree1@gmail.com>
2026-02-20 15:31:39 +0000

    Add Microsoft Clarity script to global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,13 @@ export default function RootLayout({
 
   gtag('config', 'G-1WFFEZEZVM');
 `}</script>
+        <script>{`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vke73owskm");
+`}</script>
       </head>
       <body className={`${spaceGrotesk.variable} ${vt323.variable}`}>
         {children}
